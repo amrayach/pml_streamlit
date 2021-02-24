0635534a531e124f5302804ac9111cfdde322307
amro.hendawi <amro.hendawi@campus.tu-berlin.de>
2021-02-24 13:20:45 +0100

    added slider widget for most popular words

--- app.py
+++ app.py
@@ -20,6 +20,10 @@ st.title('Character-Level CNN Predict & Explain:')
 st.text('Select Model:')
 model_in = st.selectbox('Models:', ['Yelp-Review-Polarity', 'AG-News-Category-Classifier'], index=0)
 
+# widget slider for choosing number of top n important words in making decision
+slider_range=50
+slider_start_value=5
+top_n_words = 5
 
 sentence = st.text_input('Enter Sentence:', value="Like any Barnes & Noble, it has a nice comfy cafe, and a large selection of books.  The staff is very friendly and helpful.  They stock a decent selection, and the prices are pretty reasonable.  Obviously it's hard for them to compete with Amazon.  However since all the small shop bookstores are gone, it's nice to walk into one every once in a while.")
 
@@ -72,6 +76,9 @@ doc = Doc(deploy.nlp.vocab, words=words, spaces=spaces)
 
 ents = []
 tags = []
+
+
+
 for j, i in enumerate(doc):
     new_ent = Span(doc, j, j + 1, label=str(j))
     ents.append(new_ent)
@@ -79,9 +86,6 @@ for j, i in enumerate(doc):
 
 doc.ents = []
 doc.ents = ents
-
-sum = sum(vals)
-vals = [v * 100 / sum for v in vals]
 
 col_library = {'positive': '#FF0000', 'negative': '#0000FF'}
 colors = [to_rgba(col_library['positive'], x) if x >= 0 else to_rgba(col_library['negative'], x) for x in vals]
@@ -97,8 +101,18 @@ for i in range(len(heatmap)):
 heatmap_neg = list(sorted(list(filter(lambda x: x[1] < 0, heatmap)), key=lambda x: x[1]))
 heatmap_pos = list(sorted(list(filter(lambda x: x[1] >= 0, heatmap)), key=lambda x: x[1], reverse=True))
 
+visualize_ner(doc, labels=tags, colors=col_dict, show_table=False, title='Character2Word Attention Heatmap:')
 
+top_n_words = st.slider('Choose the number of most popular words in text', 0, slider_range, slider_start_value if slider_start_value<=slider_range else slider_range)
 
-visualize_ner(doc, labels=tags, colors=col_dict, title='Character2Word Attention Heatmap:')
+def get_top_idx(vals, top_n_words):
+    idx = sorted(range(len(vals)), key=lambda i: vals[i])[-top_n_words:]
+    print("for ", top_n_words)
+    print("got ", idx)
+    return idx
+idx_most_pop_words = get_top_idx(vals, top_n_words)
+
+tags = [tags[i] for i in idx_most_pop_words]
+visualize_ner(doc, labels=tags, colors=col_dict, show_table=True, title='most popular words:')
 
 
